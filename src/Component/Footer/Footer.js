6476e97370e1760495bbca8cd96beffa0edf51c4
meriem <elounimeriem@gmail.com>
2022-11-21 12:47:15 +0100

    first commit

--- src/Component/Footer/Footer.js
+++ src/Component/Footer/Footer.js
@@ -1,11 +1,11 @@
 import React from "react";
 import "./Footer.css";
 const Footer = () => {
-  return (
-    <footer className="footer">
-      <h3>movie_app session december!</h3>
-    </footer>
-  );
+	return (
+		<footer className='footer'>
+			<h3>movie_app session Septembre © !</h3>
+		</footer>
+	);
 };
 
 export default Footer;
